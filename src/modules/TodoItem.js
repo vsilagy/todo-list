db4346310b449a48a264e15ce152c4ac07c585e5
Vlad Silagy <vladsilagy@gmail.com>
2023-11-15 23:54:41 -0500

    add update details and format date to todoItem

--- src/modules/TodoItem.js
+++ src/modules/TodoItem.js
@@ -1,3 +1,4 @@
+import { formatDate } from '../utilities/formatDate';
 export class TodoItem {
 	constructor(title, description, dueDate, priority, notes) {
 		this.title = title;
@@ -6,5 +7,22 @@ export class TodoItem {
 		this.priority = priority;
 		this.notes = notes;
 		this.completed = false;
+	}
+
+	// Toggle the status
+	toggleCompleted() {
+		this.completed = !this.completed;
+	}
+	// Update the details
+	updateDetails({ title, description, dueDate, priority, notes }) {
+		if (title !== undefined) this.title = title;
+		if (description !== undefined) this.description = description;
+		if (dueDate !== undefined) this.dueDate = dueDate;
+		if (priority !== undefined) this.priority = priority;
+		if (notes !== undefined) this.notes = notes;
+	}
+	// Formats the due date
+	getFormattedDueDate() {
+		return formatDate(this.dueDate);
 	}
 }
